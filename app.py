302143a58db4393e7d22078d86292e5907507ab8
Manavi <58180058+Manavi18@users.noreply.github.com>
2021-01-18 14:53:07 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+import requests
+# from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET','POST'])
+def home():
+    if request.method == 'POST':
+        c = request.form.get('city')
+    else:
+        c = 'Mumbai'
+
+    url = 'http://api.openweathermap.org/data/2.5/weather?units=imperial&q={}&appid=715ae271aba5e6fb65473723fa58e745'
+
+    r = requests.get(url.format(c)).json()
+    print(r)
+
+
+    weather = {
+        'city': c,
+        'temperature': r['main']['temp'],
+        'description':  r['weather'][0]['description'],
+        'icon': r['weather'][0]['icon'],
+    }
+
+    print(weather)
+    return render_template('weather.html', weather = weather)
+
+if __name__ == "__main__":
+    app.run(debug=True)
